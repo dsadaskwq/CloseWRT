e218590f925684dee3023d27a5d3a0bbfae30d4f
dsadaskwq <111348232+dsadaskwq@users.noreply.github.com>
2024-08-28 17:25:13 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -106,7 +106,7 @@ UPDATE_PACKAGE "padavanonly/luci-app-mwan3helper-chinaroute" "main"
 UPDATE_PACKAGE "tty228/luci-app-wechatpush" "master"
 UPDATE_PACKAGE "UnblockNeteaseMusic/luci-app-unblockneteasemusic" "$([[ $REPO_URL == *"lede"* ]] && echo "master" || echo "js")"
 UPDATE_PACKAGE "sirpdboy/netspeedtest" "master"
-UPDATE_PACKAGE "sirpdboy/luci-app-parentcontrol" "main"
+#UPDATE_PACKAGE "sirpdboy/luci-app-parentcontrol" "main"
 
 #更新软件包版本
 UPDATE_VERSION() {
