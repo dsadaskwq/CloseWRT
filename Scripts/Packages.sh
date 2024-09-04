8ac50188ee8b5f3ec81a30a087e7666ce24d892a
dsadaskwq <111348232+dsadaskwq@users.noreply.github.com>
2024-09-04 19:49:55 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -2,9 +2,9 @@
 #安装和更新软件包
 
 #删除自带软件包 （优先编译自带，所以需要删除才能升级）
-rm -rf $(find ../feeds/luci/ -type d -regex ".*\(luci-app-zerotier\|luci-theme-argon\|luci-app-ssr-plus\|passwall\|aliyundrive-webdav\|openclash\|mosdns\|dockerman\|adguardhome\|alist\|luci-app-unblockneteasemusic\).*")
+rm -rf $(find ../feeds/luci/ -type d -regex ".*\(luci-theme-argon\|luci-app-ssr-plus\|passwall\|aliyundrive-webdav\|openclash\|mosdns\|dockerman\|adguardhome\|alist\|luci-app-unblockneteasemusic\).*")
 #删除自带核心packages 
-rm -rf $(find ../feeds/packages/ -type d -regex ".*\(zerotier\|samba4\|alist\|golang\|mosdns\|chinadns-ng\|sing-box\|xray-core\|v2ray-core\|v2ray-plugin\|v2ray-geodata\|aliyundrive-webdav\).*")
+rm -rf $(find ../feeds/packages/ -type d -regex ".*\(samba4\|alist\|golang\|mosdns\|chinadns-ng\|sing-box\|xray-core\|v2ray-core\|v2ray-plugin\|v2ray-geodata\|aliyundrive-webdav\).*")
 
 ##git仓库  "$4"可拉仓库子目录
 UPDATE_PACKAGE() {
